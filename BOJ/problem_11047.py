7fa1bc6ab41d42f8039c120167e3ca8c90b235db
Jordan Bae <qoentlr37@naver.com>
2021-07-18 18:06:33 +0900

    add BOJ/problem_11047.py

---
+++ BOJ/problem_11047.py
@@ -0,0 +1,26 @@
+'''
+준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+
+동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+
+첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
+
+둘째 줄부터 N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
+
+
+'''
+
+n, total = map(int, input().split())
+coin_set = [int(input()) for _ in range(n)]
+
+result = 0
+
+for coin in coin_set[::-1]:
+    if not total > 0:
+        break
+
+    if total >= coin:
+        result += (total // coin)
+        total = total % coin
+
+print(result)
